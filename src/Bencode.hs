39b32f0944298006b162a657c4095c745f9b7820
Aaron Smith <asmith1810@gmail.com>
2015-06-27 22:37:03 -0600

    Functions for decoding ints and strings. Decoding not yet implemented.

--- src/Bencode.hs
+++ src/Bencode.hs
@@ -19,7 +19,7 @@ class Encode a where
     encode :: a -> String
     -- |Given a String, parse the string into the resulting set of Bencode
     -- values.
-    decode :: String -> a
+    decode :: String -> Either String a
 
 instance Encode BType where
     -- |Encoding integers.
@@ -34,7 +34,37 @@ instance Encode BType where
     encode (BList xs) = "l" ++ foldl (\a x -> a ++ encode x) "" xs ++ "e"
 
     -- |Encoding dictionaries.
-    encode (BDict xs) = "d" ++ foldl (\a x -> a ++ encode (BString $ fst x) ++ encode (snd x)) "" xs ++ "e"
+    encode (BDict xs) = "d" ++ foldDict ++ "e"
+        where foldDict = foldl (\a x -> a ++ encode (BString $ fst x) ++ encode (snd x)) "" xs
 
-    -- |Decoding integers.
-    decode s = BInteger 5
+    -- |Decoding strings into BTypes.
+    decode = decode'
+        where decode' acc a = case decodeVal acc a of
+                                Left msg -> Left msg
+                                Right val -> Right val
+              decodeVal acc s@(x:xs)
+                    | x == 'i' = Right $ BInteger 10
+                    | x == 'l' = Right $ BList []
+                    | x == 'd' = Right $ BDict []
+                    | otherwise = Right $ BString "foo"
+
+-- |Parse a string from a Bencoded string and return it, along with the
+-- remaining portion of the original Bencoded string.
+parseString :: String -> (String, String)
+parseString s = let len = read (takeWhile (/= ':') s) :: Int
+                    rest = drop 1 $ dropWhile (/= ':') s -- drop through ':'
+                in parseString' len rest
+    where parseString' len rest = (take len rest, drop len rest)
+
+-- |Parse an integer from a Bencoded string and return it, along with the
+-- remaining portion of the original Bencoded string.
+parseInt :: String -> Either String (Integer, String)
+parseInt xs =
+    let parsed = reads (takeWhile (/= 'e') xs) :: [(Int, String)]
+    -- |Integer must contain only numbers.
+    in case parsed of
+         [] -> Left "Invalid characters in decoded integer."
+         [(i, s)] -> let rest = drop (i + 1) xs -- drop through 'e'
+                     in if not $ null s
+                          then Left "Invalid characters in decoded integer."
+                          else Right (toInteger i, rest)
--- test/BencodeSpec.hs
+++ test/BencodeSpec.hs
@@ -3,65 +3,87 @@ import Test.Hspec
 
 main :: IO ()
 main = hspec $ do
-  describe "Encoding" $ do
-    describe "Integer encoding" $ do
-      it "should encode integers" $ do
-        let exp = "i3e"
-        encode (BInteger 3) `shouldBe` exp
-      it "should encode negative integers" $ do
-        let exp = "i-3e"
-        encode (BInteger (-3)) `shouldBe` exp
-      it "should not pad integers with 0s" $ do
-        let exp = "i3e"
-        encode (BInteger 0003) `shouldBe` exp
+  describe "Integer encoding and decoding" $ do
+    it "should encode and decode integers" $ do
+      let exp = "i3e"
+      let bin = BInteger 3
+      encode bin `shouldBe` exp
+      decode exp `shouldBe` bin
+    it "should encode and decode negative integers" $ do
+      let exp = "i-3e"
+          bin = BInteger (-3)
+      encode bin `shouldBe` exp
+      decode exp `shouldBe` bin
+    it "should not pad integers with 0s" $ do
+      let exp = "i3e"
+          bin = BInteger 0003
+      encode bin `shouldBe` exp
+      decode exp `shouldBe` bin
   
-    describe "String encoding" $ do
-      it "should encode strings" $ do
-        let exp = "4:spam"
-        encode (BString "spam") `shouldBe` exp
-      it "should encode the empty string" $ do
-        let exp = "0:"
-        encode (BString "") `shouldBe` exp
+  describe "String encoding and decoding" $ do
+    it "should encode and decode strings" $ do
+      let exp = "4:spam"
+          bin = BString "spam"
+      encode bin `shouldBe` exp
+      decode exp `shouldBe` bin
+    it "should encode and decode the empty string" $ do
+      let exp = "0:"
+          bin = BString ""
+      encode bin `shouldBe` exp
+      decode exp `shouldBe` bin
   
-    describe "List encoding" $ do
-      it "should encode lists of one type" $ do
-        let exp = "li3ei4ei5ee"
-        encode (BList [BInteger 3, BInteger 4, BInteger 5]) `shouldBe` exp
-      it "should encode lists of mixed type" $ do
-        let exp = "l4:spami3e4:eggse"
-        encode (BList [BString "spam", BInteger 3, BString "eggs"]) `shouldBe` exp
-      it "should encode an empty list" $ do
-        let exp = "le"
-        encode (BList []) `shouldBe` exp
-      it "should encode a list of lists" $ do
-        let exp = "l4:spamli3ei4eee"
-        encode (BList [BString "spam", BList [BInteger 3, BInteger 4]]) `shouldBe` exp
-      it "should encode a list of dictionaries" $ do
-        let exp = "ld3:cow3:mooed3:cow3:mooee"
-        encode (BList [BDict [("cow", BString "moo")], 
-                      BDict [("cow", BString "moo")]]) `shouldBe` exp
+  describe "List encoding and decoding" $ do
+    it "should encode and decode lists of one type" $ do
+      let exp = "li3ei4ei5ee"
+          bin = BList [BInteger 3, BInteger 4, BInteger 5]
+      encode bin `shouldBe` exp
+      decode exp `shouldBe` bin
+    it "should encode and decode lists of mixed type" $ do
+      let exp = "l4:spami3e4:eggse"
+          bin = BList [BString "spam", BInteger 3, BString "eggs"]
+      encode bin `shouldBe` exp
+      decode exp `shouldBe` bin
+    it "should encode and decode an empty list" $ do
+      let exp = "le"
+          bin = BList []
+      encode bin `shouldBe` exp
+      decode exp `shouldBe` bin
+    it "should encode and decode a list of lists" $ do
+      let exp = "l4:spamli3ei4eee"
+          bin = BList [BString "spam", BList [BInteger 3, BInteger 4]]
+      encode bin `shouldBe` exp
+      decode exp `shouldBe` bin
+    it "should encode and decode a list of dictionaries" $ do
+      let exp = "ld3:cow3:mooed3:cow3:mooee"
+          bin = BList [BDict [("cow", BString "moo")], 
+                        BDict [("cow", BString "moo")]]
+      encode bin `shouldBe` exp
+      decode exp `shouldBe` bin
   
-    describe "Dictionary encoding" $ do
-      it "should encode a dictionary with an integer value" $ do
-        let exp = "d3:fooi3ee"
-        encode (BDict [("foo", BInteger 3)]) `shouldBe` exp
-      it "should encode a dictionary with a string value" $ do
-        let exp = "d3:foo3:bare"
-        encode (BDict [("foo", BString "bar")]) `shouldBe` exp
-      it "should encode a dictionary with a list value" $ do
-        let exp = "d3:fooli3eee"
-        encode (BDict [("foo", BList [BInteger 3])]) `shouldBe` exp
-      it "should encode a dictionary with a dictionary value" $ do
-        let exp = "d3:food3:bari3eee"
-        encode (BDict [("foo", BDict [("bar", BInteger 3)])]) `shouldBe` exp
-      it "should encode an empty dictionary" $ do
-        let exp = "de"
-        encode (BDict []) `shouldBe` exp
-
-  describe "Decoding" $ do
-    describe "Integer decoding" $ do
-      it "should decode an integer" $ do
-        let exp = BInteger 3
-        decode "i3e" `shouldBe` exp
-      
+  describe "Dictionary encoding and decoding" $ do
+    it "should encode and decode a dictionary with an integer value" $ do
+      let exp = "d3:fooi3ee"
+          bin = BDict [("foo", BInteger 3)]
+      encode bin `shouldBe` exp
+      decode exp `shouldBe` bin
+    it "should encode and decode a dictionary with a string value" $ do
+      let exp = "d3:foo3:bare"
+          bin = BDict [("foo", BString "bar")]
+      encode bin `shouldBe` exp
+      decode exp `shouldBe` bin
+    it "should encode and decode a dictionary with a list value" $ do
+      let exp = "d3:fooli3eee"
+          bin = BDict [("foo", BList [BInteger 3])]
+      encode bin `shouldBe` exp
+      decode exp `shouldBe` bin
+    it "should encode and decode a dictionary with a dictionary value" $ do
+      let exp = "d3:food3:bari3eee"
+          bin = BDict [("foo", BDict [("bar", BInteger 3)])]
+      encode bin `shouldBe` exp
+      decode exp `shouldBe` bin
+    it "should encode and decode an empty dictionary" $ do
+      let exp = "de"
+          bin = BDict []
+      encode bin `shouldBe` exp      
+      decode exp `shouldBe` bin
 
