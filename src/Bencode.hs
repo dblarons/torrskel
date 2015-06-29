49f5e296824a358f4449a080bdb41ddd92ed1e2d
Aaron Smith <asmith1810@gmail.com>
2015-06-28 18:11:58 -0600

    Return rest of string from dict and list funs. Abstract decode so it can be recursive. Functional.

--- src/Bencode.hs
+++ src/Bencode.hs
@@ -3,12 +3,15 @@ module Bencode
     ( Encode,
       encode,
       decode,
+      BError,
       BType(BInteger, BString, BList, BDict)
       ) where
 
+type BError = String
+
 data BType
     = BString   String
-    | BList    [BType]
+    | BList     [BType]
     | BInteger  Integer
     | BDict     [(String, BType)]
     deriving (Eq, Show)
@@ -17,9 +20,6 @@ class Encode a where
     -- |Given a type that can be Bencoded, invoke the correct encoding
     -- function.
     encode :: a -> String
-    -- |Given a String, parse the string into the resulting set of Bencode
-    -- values.
-    decode :: String -> Either String a
 
 instance Encode BType where
     -- |Encoding integers.
@@ -37,34 +37,87 @@ instance Encode BType where
     encode (BDict xs) = "d" ++ foldDict ++ "e"
         where foldDict = foldl (\a x -> a ++ encode (BString $ fst x) ++ encode (snd x)) "" xs
 
-    -- |Decoding strings into BTypes.
-    decode = decode'
-        where decode' acc a = case decodeVal acc a of
-                                Left msg -> Left msg
-                                Right val -> Right val
-              decodeVal acc s@(x:xs)
-                    | x == 'i' = Right $ BInteger 10
-                    | x == 'l' = Right $ BList []
-                    | x == 'd' = Right $ BDict []
-                    | otherwise = Right $ BString "foo"
+decode :: String -> Either BError BType
+decode s = let val = rDecode s
+           in case val of
+                Left msg -> Left msg
+                Right v -> let (result, rest) = v
+                           in if rest /= ""
+                                then Left "String was not entirely decoded."
+                                else Right result
+
+-- |Given a String, parse the string into the resulting set of Bencode
+-- values.
+rDecode :: String -> Either BError (BType, String)
+rDecode s@(x:xs)
+    | x == 'i' = case parseInt s of
+                   Left msg -> Left msg
+                   Right val -> Right (BInteger val, dropInt s)
+    | x == 'l' = parseList xs []
+    | x == 'd' = parseDict xs []
+    | otherwise = case parseString s of
+                    Left msg -> Left msg
+                    Right val -> Right (BString val, dropString s (length val))
+
+parseDict :: String -> [(String, BType)] -> Either String (BType, String)
+parseDict s@(x:xs) acc
+    | x == 'e' = Right (BDict acc, xs)
+    | otherwise = let key = parseString s
+                  in case key of
+                       Left msg -> Left msg
+                       Right k -> let val = rDecode $ dropString s (length k)
+                                  in case val of
+                                       Left msg -> Left msg
+                                       Right v -> let (value, rest) = v
+                                                  in parseDict rest (acc ++ [(k, value)])
+
+parseList :: String -> [BType] -> Either String (BType, String)
+parseList s@(x:xs) acc
+    | x == 'e' = Right (BList acc, xs)
+    | x == 'd' || x == 'l' = let val = rDecode s
+                             in case val of
+                                  Left msg -> Left msg
+                                  Right val -> let (v, rest) = val
+                                               in parseList rest (acc ++ [v])
+    | x == 'i' = let rest = dropInt s
+                 in case parseInt s of
+                      Left msg -> Left msg
+                      Right val -> parseList rest (acc ++ [BInteger val])
+    | otherwise = case parseString s of
+                    Left msg -> Left msg
+                    Right val -> let rest = dropString s (length val)
+                                 in parseList rest (acc ++ [BString val])
 
 -- |Parse a string from a Bencoded string and return it, along with the
 -- remaining portion of the original Bencoded string.
-parseString :: String -> (String, String)
-parseString s = let len = read (takeWhile (/= ':') s) :: Int
-                    rest = drop 1 $ dropWhile (/= ':') s -- drop through ':'
-                in parseString' len rest
-    where parseString' len rest = (take len rest, drop len rest)
+parseString :: String -> Either BError String
+parseString s = let maybeVal = readNumUntilChar s ':'
+                    rest = dropUntil (/= ':') s -- drop through ':'
+                in case maybeVal of
+                     Nothing -> Left "Invalid characters in decoded string length."
+                     Just len -> Right $ take len rest
+
+-- |Drop n the string of length n from the Bencoded value.
+dropString :: String -> Int -> String
+dropString s n = drop n $ dropUntil (/= ':') s
 
 -- |Parse an integer from a Bencoded string and return it, along with the
 -- remaining portion of the original Bencoded string.
-parseInt :: String -> Either String (Integer, String)
-parseInt xs =
-    let parsed = reads (takeWhile (/= 'e') xs) :: [(Int, String)]
-    -- |Integer must contain only numbers.
-    in case parsed of
-         [] -> Left "Invalid characters in decoded integer."
-         [(i, s)] -> let rest = drop (i + 1) xs -- drop through 'e'
-                     in if not $ null s
-                          then Left "Invalid characters in decoded integer."
-                          else Right (toInteger i, rest)
+parseInt :: String -> Either BError Integer
+parseInt (x:xs) =
+    let maybeVal = readNumUntilChar xs 'e'
+    in case maybeVal of
+         Nothing -> Left "Invalid characters in decoded integer."
+         Just i -> Right $ toInteger i
+
+dropInt :: String -> String
+dropInt = dropUntil (/= 'e')
+
+readNumUntilChar :: String -> Char -> Maybe Int
+readNumUntilChar s c = let val = reads (takeWhile (/= c) s) :: [(Int, String)]
+                       in case val of
+                            [(i, "")] -> Just i
+                            otherwise -> Nothing
+
+dropUntil :: (a -> Bool) -> [a] -> [a]
+dropUntil f xs = drop 1 $ dropWhile f xs
--- test/BencodeSpec.hs
+++ test/BencodeSpec.hs
@@ -8,82 +8,82 @@ main = hspec $ do
       let exp = "i3e"
       let bin = BInteger 3
       encode bin `shouldBe` exp
-      decode exp `shouldBe` bin
+      decode exp `shouldBe` Right bin
     it "should encode and decode negative integers" $ do
       let exp = "i-3e"
           bin = BInteger (-3)
       encode bin `shouldBe` exp
-      decode exp `shouldBe` bin
+      decode exp `shouldBe` Right bin
     it "should not pad integers with 0s" $ do
       let exp = "i3e"
           bin = BInteger 0003
       encode bin `shouldBe` exp
-      decode exp `shouldBe` bin
+      decode exp `shouldBe` Right bin
   
   describe "String encoding and decoding" $ do
     it "should encode and decode strings" $ do
       let exp = "4:spam"
           bin = BString "spam"
       encode bin `shouldBe` exp
-      decode exp `shouldBe` bin
+      decode exp `shouldBe` Right bin
     it "should encode and decode the empty string" $ do
       let exp = "0:"
           bin = BString ""
       encode bin `shouldBe` exp
-      decode exp `shouldBe` bin
+      decode exp `shouldBe` Right bin
   
   describe "List encoding and decoding" $ do
     it "should encode and decode lists of one type" $ do
       let exp = "li3ei4ei5ee"
           bin = BList [BInteger 3, BInteger 4, BInteger 5]
       encode bin `shouldBe` exp
-      decode exp `shouldBe` bin
+      decode exp `shouldBe` Right bin
     it "should encode and decode lists of mixed type" $ do
       let exp = "l4:spami3e4:eggse"
           bin = BList [BString "spam", BInteger 3, BString "eggs"]
       encode bin `shouldBe` exp
-      decode exp `shouldBe` bin
+      decode exp `shouldBe` Right bin
     it "should encode and decode an empty list" $ do
       let exp = "le"
           bin = BList []
       encode bin `shouldBe` exp
-      decode exp `shouldBe` bin
+      decode exp `shouldBe` Right bin
     it "should encode and decode a list of lists" $ do
       let exp = "l4:spamli3ei4eee"
           bin = BList [BString "spam", BList [BInteger 3, BInteger 4]]
       encode bin `shouldBe` exp
-      decode exp `shouldBe` bin
+      decode exp `shouldBe` Right bin
     it "should encode and decode a list of dictionaries" $ do
       let exp = "ld3:cow3:mooed3:cow3:mooee"
           bin = BList [BDict [("cow", BString "moo")], 
                         BDict [("cow", BString "moo")]]
       encode bin `shouldBe` exp
-      decode exp `shouldBe` bin
+      decode exp `shouldBe` Right bin
   
   describe "Dictionary encoding and decoding" $ do
     it "should encode and decode a dictionary with an integer value" $ do
       let exp = "d3:fooi3ee"
           bin = BDict [("foo", BInteger 3)]
       encode bin `shouldBe` exp
-      decode exp `shouldBe` bin
+      decode exp `shouldBe` Right bin
     it "should encode and decode a dictionary with a string value" $ do
       let exp = "d3:foo3:bare"
           bin = BDict [("foo", BString "bar")]
       encode bin `shouldBe` exp
-      decode exp `shouldBe` bin
+      decode exp `shouldBe` Right bin
     it "should encode and decode a dictionary with a list value" $ do
       let exp = "d3:fooli3eee"
           bin = BDict [("foo", BList [BInteger 3])]
       encode bin `shouldBe` exp
-      decode exp `shouldBe` bin
+      decode exp `shouldBe` Right bin
     it "should encode and decode a dictionary with a dictionary value" $ do
       let exp = "d3:food3:bari3eee"
           bin = BDict [("foo", BDict [("bar", BInteger 3)])]
       encode bin `shouldBe` exp
-      decode exp `shouldBe` bin
+      decode exp `shouldBe` Right bin
     it "should encode and decode an empty dictionary" $ do
       let exp = "de"
           bin = BDict []
       encode bin `shouldBe` exp      
-      decode exp `shouldBe` bin
+      decode exp `shouldBe` Right bin
 
