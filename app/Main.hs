9b6e751405160233ae7bc238c79d484a19eb4477
Aaron Smith <aaronsmith.dev@gmail.com>
2017-12-20 18:19:52 -0800

    Switch exposed-modules to other-modules for test suite

--- app/Main.hs
+++ app/Main.hs
@@ -5,12 +5,12 @@ module Main where
 import Control.Monad.Trans.Except
 import Network.Wreq
 
-import MetaInfoFile
 import Bencode
+import MetaInfoFile
 
 main :: IO ()
 main = do
-    contents <- runExceptT $ readTorrentFile "bin/test.torrent"
-    case contents of
-      Left msg -> putStrLn msg
-      Right val -> print val
+  contents <- runExceptT $ readTorrentFile "bin/test.torrent"
+  case contents of
+    Left msg -> putStrLn msg
+    Right val -> print val
